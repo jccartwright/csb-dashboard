5236b5869db5e705285945a4d52774149041bbf4
jccartwright <john.c.cartwright@comcast.net>
2023-03-27 16:43:32 -0600

    panels layout with placeholder components

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,63 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HeaderPanel></HeaderPanel>
+      <SidePanel></SidePanel>
+      <MapPanel></MapPanel>
+      <LineGraphPanel></LineGraphPanel>
+      <FooterPanel></FooterPanel>
     </div>
   )
 }
 
 export default App
+
+function HeaderPanel() {
+  const baseClass = 'HeaderPanel'
+  
+  return (
+    <div className={baseClass}>
+      <header><h2>HEADER</h2></header>
+    </div>
+  )
+}
+
+function SidePanel() {
+  const baseClass = 'SidePanel'
+
+  return (
+    <div className={baseClass}>
+      <h2>SIDEPANEL</h2>
+    </div>
+  )
+}
+
+function MapPanel() {
+  const baseClass = 'MapPanel'
+  
+  return (
+    <div className={baseClass}>
+      <h2>MAPPANEL</h2>
+    </div>
+  )
+}
+
+function LineGraphPanel() {
+  const baseClass = 'LineGraphPanel'
+
+  return (
+    <div className={baseClass}>
+      <h2>LINEGRAPHPANEL</h2>
+    </div>
+  )
+}
+
+function FooterPanel() {
+  const baseClass = 'FooterPanel'
+
+  return (
+    <div className={baseClass}>
+      <footer><h2>FOOTER</h2></footer>
+    </div>
+  )
+}
