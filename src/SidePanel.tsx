d277109b7766e71a97c517de333df3612ba3153b
jccartwright <john.c.cartwright@comcast.net>
2023-04-29 12:56:09 -0600

    temporarily turn off tsonfig 'strict'

--- src/SidePanel.tsx
+++ src/SidePanel.tsx
@@ -1,7 +1,7 @@
 import './SidePanel.css'
 
-
-export default function SidePanel({children}: Props) {
+// export default function SidePanel({children}: Props) {
+export default function SidePanel({children}) {
   const baseClass = 'SidePanel'
   // console.log(query?.data)
   // if (query.data) { console.log(`data: {query.data}`) }
